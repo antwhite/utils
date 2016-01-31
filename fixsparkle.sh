66ed3510545b067ebdade8016ac665ba7162712f
Ant White <acwhite@opentable.com>
2016-01-31 18:07:17 +0000

    tabs

--- fixsparkle.sh
+++ fixsparkle.sh
@@ -5,39 +5,39 @@ RED='\033[0;31m'
 NC='\033[0m' 
  
 mdfind "kMDItemKind == 'Application'" |grep -vi xcode | grep -vi xamarin | while read line ; do 
-	domain="$line/Contents/Info.plist"
+    domain="$line/Contents/Info.plist"
     if [ ! -f "$domain" ]
-	then
-  		continue;
-	fi
-	
+    then
+        continue;
+    fi
+    
     sparkelUrl=$(defaults read $domain SUFeedURL 2>/dev/null)
     if [ -z "$sparkelUrl" ]; then
-    	continue
-	fi
-	echo "Found $domain using sparkle"
-	if [[ $sparkelUrl =~ ^https ]]; then
-		echo -e "${GREEN}Using secure $sparkelUrl${NC}"
-		continue
-	fi
-	echo -e "${RED}Using insecure $sparkelUrl${NC} Attempting to Patch..."
-	httpsLink=${sparkelUrl/http:/https:}
-	$(curl -I $httpsLink &>/dev/null)
-	if [ $? -ne 0 ]; then
-		echo -e "${RED}Cannot patch contact developer${NC}"
-		continue
-	fi 
-	echo "Patching..."
-	echo "defaults write $domain SUFeedURL $sparkelUrl" >> revert.sh
-	defaults write $domain SUFeedURL $httpsLink
-	if [ $? -ne 0 ]; then
-		echo "${RED}Something went tits up"
-		continue
-	fi
-	$(codesign -dv $line &>/dev/null)
-	if [ $? -ne 0 ]; then
-		echo "${RED}Something went tits up"
-		continue
-	fi 
-	echo -e "${GREEN} Patched${NC}"
+        continue
+    fi
+    echo "Found $domain using sparkle"
+    if [[ $sparkelUrl =~ ^https ]]; then
+        echo -e "${GREEN}Using secure $sparkelUrl${NC}"
+        continue
+    fi
+    echo -e "${RED}Using insecure $sparkelUrl${NC} Attempting to Patch..."
+    httpsLink=${sparkelUrl/http:/https:}
+    $(curl -I $httpsLink &>/dev/null)
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Cannot patch contact developer${NC}"
+        continue
+    fi 
+    echo "Patching..."
+    echo "defaults write $domain SUFeedURL $sparkelUrl" >> revert.sh
+    defaults write $domain SUFeedURL $httpsLink
+    if [ $? -ne 0 ]; then
+        echo "${RED}Something went tits up"
+        continue
+    fi
+    $(codesign -dv $line &>/dev/null)
+    if [ $? -ne 0 ]; then
+        echo "${RED}Something went tits up"
+        continue
+    fi 
+    echo -e "${GREEN} Patched${NC}"
 done
